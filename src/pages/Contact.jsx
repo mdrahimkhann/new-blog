d75d5e4378b49e5ab64d6c2505b01f2be250cac2
Md Rahim Khan <mdrahimkhanm6@gmail.com>
2025-03-14 09:18:05 +0200

    first commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,21 +17,12 @@ const Contact = () => {
   };
 
   return (
-    <div className="w-screen h-screen lg:flex items-center justify-center sm:grid-cols-1   bg-gray-100 p-6">
-       <div className="min-h-80 bg-gray-400 max-w-md space-y-4 w-full p-5">
-         <h1>ACHINEVEMENT</h1>
-       </div>
-      <div className="bg-white shadow-lg rounded-xl p-6 max-w-md w-full ">
-       
-
-
-
-
-
+    <div className="">
+  <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+      <div className="bg-white shadow-lg rounded-xl p-6 max-w-md w-full">
         <h2 className="text-2xl font-semibold text-center text-gray-800 mb-4">
           Contact Us
         </h2>
-       
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700">
@@ -82,33 +73,8 @@ const Contact = () => {
             Send Message
           </button>
         </form>
-
-
-
-
-       
-
-
-
-
-
-
       </div>
-
-
-
-
-
-      <>
-        
-        <div className="min-h-80 lg:min-h-80 bg-fuchsia-500  max-w-md w-full">
-          <h1>MORE TO WAYA CONTACT</h1>
-
-
-
-
-        </div>
-        </>
+    </div>
 
     </div>
   );
